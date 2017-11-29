e08a5582d575254eb6418f6dfbaf9f878b62f4f9
Scott Ouellette <scottx611x@gmail.com>
2017-11-29 16:08:47 -0500

    add test

--- tests.py
+++ tests.py
@@ -1,8 +1,14 @@
+import os
 import unittest
+
+from ps_grapher import PsGrapher
 
 
 class PsGrapherTests(unittest.TestCase):
-    pass
+
+    def test_html_file_created(self):
+        PsGrapher(iterations=2, time_interval=2).run()
+        os.remove("MemoryUsage.html")
 
 
 if __name__ == "__main__":
