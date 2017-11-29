336aea778b9aa1b13c9a1fd4991d8b6ce106ad65
Scott Ouellette <scottx611x@gmail.com>
2017-11-29 15:31:58 -0500

    use OO aproach for easier testing

--- ps_grapher.py
+++ ps_grapher.py
@@ -7,100 +7,98 @@ from subprocess import check_output
 import sys
 import os
 
-PS_DATA = {}
-GRAPH_DATA = {}
-TIME_INTERVAL = 60  # seconds
 
+class PsGrapher:
 
-def get_ps_output():
-    output = [item.lstrip().rstrip() for item in check_output(
-        "ps -e -o pid,%cpu,%mem,rss,command --sort -rss".split(" ")).split("\n")]
-    output.pop(0)
-    new = [a.split() for a in output[:10] if a]
-    newer = [[x[0], x[1], x[2], x[3], " ".join(x[4:])] for x in new]
-    return newer
+    def __init__(self):
+        self.ps_data = {}
+        self.graph_data = {}
+        self.time_interval = 60  # seconds
 
+    def get_ps_output(self):
+        output = [item.lstrip().rstrip() for item in check_output(
+            "ps -e -o pid,%cpu,%mem,rss,command --sort -rss".split(" ")).split("\n")]
+        output.pop(0)
+        new = [a.split() for a in output[:10] if a]
+        newer = [[x[0], x[1], x[2], x[3], " ".join(x[4:])] for x in new]
+        return newer
 
-def create_layout(title):
-    return dict(
-        title=title,
-        xaxis=dict(
-            rangeselector=dict(
-                buttons=list([
-                    dict(
-                        count=1,
-                        label='1 min',
-                        step='minute',
-                        stepmode='backward'
-                    ),
-                    dict(
-                        count=1,
-                        label='1 hour',
-                        step='hour',
-                        stepmode='backward'
-                    ),
-                    dict(
-                        count=1,
-                        label='1 day',
-                        step='day',
-                        stepmode='backward'
-                    ),
-                    dict(step='all')
-                ])
-            ),
-            rangeslider=dict(),
-            type='date',
-            tickformat='%H:%M:%S'
-        )
-    )
-
-
-def update_graph_data():
-    for command in PS_DATA.keys():
-        dates = sorted([date for date in PS_DATA[command].keys() if not date == "trace"])
-	if not PS_DATA[command]["trace"]:
-            PS_DATA[command]["trace"] = go.Scatter(
-                x=dates,
-                y=[PS_DATA[command][date] for date in dates],
-                name=command[0:100],
-                opacity=0.8,
-                connectgaps=True,
-                line=dict(shape='spline')
+    def _create_layout(self, title):
+        return dict(
+            title=title,
+            xaxis=dict(
+                rangeselector=dict(
+                    buttons=list([
+                        dict(
+                            count=1,
+                            label='1 min',
+                            step='minute',
+                            stepmode='backward'
+                        ),
+                        dict(
+                            count=1,
+                            label='1 hour',
+                            step='hour',
+                            stepmode='backward'
+                        ),
+                        dict(
+                            count=1,
+                            label='1 day',
+                            step='day',
+                            stepmode='backward'
+                        ),
+                        dict(step='all')
+                    ])
+                ),
+                rangeslider=dict(),
+                type='date',
+                tickformat='%H:%M:%S'
             )
-        else:
-            PS_DATA[command]["trace"].x = dates
-            PS_DATA[command]["trace"].y = [PS_DATA[command][date] for date in dates]
-	GRAPH_DATA[command] = PS_DATA[command]["trace"]
+        )
 
-def update_plotly_graph(extend_existing_plot=False):
-    global GRAPH_DATA
-    global PS_DATA
-    fig = go.Figure(
-        data=GRAPH_DATA.values(),
-        layout=create_layout("Memory Usage (kb)")
-    )
-    plot(fig, filename="MemoryUsage.html", auto_open=False)
+    def run(self):
+        while True:
+            date = datetime.datetime.now()
+            for row in self.get_ps_output():
+                pid, cpu_percentage, mem_percentage, resident_set_size, process = row
+                command_id = pid + " " + process
 
-def main():
-    global INITIAL_PLOT
-    global PS_DATA
-    global TIME_INTERVAL
-    while True:
-        date = datetime.datetime.now()
-        for row in get_ps_output():
-            pid, cpu_percentage, mem_percentage, resident_set_size, process = row
-            command_id = pid + " " + process
+                if not self.ps_data.get(command_id):
+                    self.ps_data[command_id] = {
+                        date: resident_set_size,
+                        "trace": None
+                    }
+                else:
+                    self.ps_data[command_id][date] = resident_set_size
+            self.update_graph_data()
+            self.update_plotly_graph()
+            time.sleep(self.time_interval)
 
-            if not PS_DATA.get(command_id):
-                PS_DATA[command_id] = {
-                    date: resident_set_size,
-                    "trace": None
-		}
+    def update_graph_data(self):
+        for command in self.ps_data.keys():
+            dates = sorted([date for date in self.ps_data[
+                           command].keys() if not date == "trace"])
+    	if not self.ps_data[command]["trace"]:
+                self.ps_data[command]["trace"] = go.Scatter(
+                    x=dates,
+                    y=[self.ps_data[command][date] for date in dates],
+                    name=command[0:100],
+                    opacity=0.8,
+                    connectgaps=True,
+                    line=dict(shape='spline')
+                )
             else:
-                PS_DATA[command_id][date] = resident_set_size
-    	update_graph_data()
-    	update_plotly_graph()
-        time.sleep(TIME_INTERVAL)
+                self.ps_data[command]["trace"].x = dates
+                self.ps_data[command]["trace"].y = [self.ps_data[command][date] for date in dates]
+    	self.graph_data[command] = self.ps_data[command]["trace"]
+
+    def update_plotly_graph(self, extend_existing_plot=False):
+        fig = go.Figure(
+            data=self.graph_data.values(),
+            layout=self._create_layout("Memory Usage (kb)")
+        )
+        plot(fig, filename="MemoryUsage.html", auto_open=False)
+
 
 if __name__ == "__main__":
-    main()
+    PsGrapher().run()
