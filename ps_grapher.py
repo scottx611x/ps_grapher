b548385fb713b3528e3ada15e49db79609198b30
Scott Ouellette <scottx611x@gmail.com>
2017-11-29 21:58:52 -0500

    Update test coverage

--- ps_grapher.py
+++ ps_grapher.py
@@ -21,15 +21,25 @@ class PsGrapher:
         self.iterations = iterations
         self.n_top_processes = n_top_processes
         self.output_filename = "{}.html".format(output_filename)
+        self.graph_title = "Memory Usage (kb)"
 
     def _format_ps_output(self):
-        process_info_list = [process_info.split() for process_info in self.get_ps_output()[
-            :self.n_top_processes] if process_info]
+        process_info_list = [process_info.split() for process_info in self._get_ps_output()[
+            1:self.n_top_processes + 1] if process_info]
         return [
             [process_info[0], process_info[1], process_info[2],
                 process_info[3], " ".join(process_info[4:])]
             for process_info in process_info_list
         ]
+
+    def _get_date(self):
+        return datetime.datetime.now()
+
+    def _get_graph_data(self):
+        return self.graph_data
+
+    def _get_ps_data(self):
+        return self.ps_data
 
     def _get_iterations(self, count):
         if self.iterations is None:
@@ -38,14 +48,14 @@ class PsGrapher:
             return True
         return False
 
-    def get_ps_output(self):
+    def _get_ps_output(self):
         ps_command = "ps -e -o pid,%cpu,%mem,rss,command --sort -rss"
         return [item.lstrip().rstrip()
                 for item in check_output(ps_command.split(" ")).split("\n")]
 
-    def _create_layout(self, title):
+    def _create_layout(self):
         return {
-            "title": title,
+            "title": self.graph_title,
             "xaxis": {
                 "rangeselector": {
                     "buttons": [
@@ -78,24 +88,13 @@ class PsGrapher:
 
     def run(self, count=0):
         while self._get_iterations(count):
-            date = datetime.datetime.now()
-            for row in self._format_ps_output():
-                pid, cpu_percentage, mem_percentage, resident_set_size, process = row
-                command_id = pid + " " + process
-
-                if not self.ps_data.get(command_id):
-                    self.ps_data[command_id] = {
-                        date: resident_set_size,
-                        "trace": None
-                    }
-                else:
-                    self.ps_data[command_id][date] = resident_set_size
-            self.update_graph_data()
-            self.update_plotly_graph()
+            self._update_ps_data()
+            self._update_graph_data()
+            self._update_plotly_graph()
             time.sleep(self.time_interval)
             count += 1
 
-    def update_graph_data(self):
+    def _update_graph_data(self):
         for command in self.ps_data.keys():
             dates = sorted([date for date in self.ps_data[
                            command].keys() if not date == "trace"])
@@ -112,12 +111,26 @@ class PsGrapher:
             else:
                 self.ps_data[command]["trace"].x = dates
                 self.ps_data[command]["trace"].y = memory_usage
-            self.graph_data[command] = self.ps_data[command]["trace"]
+            self._get_graph_data()[command] = self.ps_data[command]["trace"]
 
-    def update_plotly_graph(self, extend_existing_plot=False):
+    def _update_ps_data(self):
+        date = self._get_date()
+        for row in self._format_ps_output():
+            pid, cpu_percentage, mem_percentage, resident_set_size, process = row
+            command_id = pid + " " + process
+
+            if not self.ps_data.get(command_id):
+                self.ps_data[command_id] = {
+                    date: resident_set_size,
+                    "trace": None
+                }
+            else:
+                self.ps_data[command_id][date] = resident_set_size
+
+    def _update_plotly_graph(self):
         fig = go.Figure(
-            data=self.graph_data.values(),
-            layout=self._create_layout("Memory Usage (kb)")
+            data=self._get_graph_data().values(),
+            layout=self._create_layout()
         )
         plot(fig, filename=self.output_filename, auto_open=False)
 
--- tests.py
+++ tests.py
@@ -1,3 +1,5 @@
+import __builtin__
+import datetime
 import mock
 import os
 import unittest
@@ -9,37 +11,136 @@ from ps_grapher import PsGrapher
 class PsGrapherTests(unittest.TestCase):
 
     def setUp(self):
+        self.fake_datetime = datetime.datetime(
+            2017, 11, 29, 21, 50, 26, 369898
+        )
+        mock.patch.object(
+            PsGrapher,
+            "_get_date",
+            return_value=self.fake_datetime
+        ).start()
+
         with open("test-data/ps_mock.txt") as f:
             self.ps_mock = mock.patch.object(
                 ps_grapher,
                 "check_output",
                 return_value=f.read()
             ).start()
-            self.ouput_filename = "Coffee"
-            self.output_file = self.ouput_filename + ".html"
+        self.maxDiff = None
+        self.output_filename = "Coffee"
+        self.output_file = self.output_filename + ".html"
+        self.ps_grapher = PsGrapher(
+            iterations=1,
+            time_interval=.01,
+            output_filename=self.output_filename,
+            n_top_processes=3
+        )
 
     def tearDown(self):
-        for path in ["PsGrapher.html", self.output_file]:
-            try:
-                os.remove(path)
-            except OSError as e:
-                pass
+        try:
+            os.remove(self.output_file)
+        except OSError as e:
+            pass
 
     def test_run_works(self):
-        PsGrapher(
-            iterations=1,
-            time_interval=1
-        ).run()
+        self.ps_grapher.run()
         self.assertTrue(self.ps_mock.called)
 
     def test_custom_filename(self):
-        PsGrapher(
-            iterations=1,
-            time_interval=1,
-            output_filename=self.ouput_filename
-        ).run()
-        self.assertTrue(self.ps_mock.called)
+        self.ps_grapher.run()
         self.assertTrue(os.path.exists(self.output_file))
+
+    def test__format_ps_output(self):
+        self.assertEqual(
+            self.ps_grapher._format_ps_output(),
+            [
+                ['5832', '0.3', '15.4', '316144',
+                    'python /vagrant/refinery/manage.py runserver 0.0.0.0:8000 --noreload'],
+                ['1246', '1.8', '8.8', '181116', '/usr/bin/java'],
+                ['2382', '4.8', '6.9', '143116',
+                    'java -server -Xss256k -Xms32m -Xmx32m']
+            ]
+        )
+
+    def test__get_ps_output(self):
+        self.assertEqual(
+            self.ps_grapher._get_ps_output(),
+            [
+                'PID %CPU %MEM   RSS COMMAND',
+                '5832  0.3 15.4 316144 python /vagrant/refinery/manage.py runserver 0.0.0.0:8000 --noreload',
+                '1246  1.8  8.8 181116 /usr/bin/java',
+                '2382  4.8  6.9 143116 java -server -Xss256k -Xms32m -Xmx32m',
+                '1257  0.0  5.9 122192 postgres: checkpointer process',
+                '5355  0.1  5.5 114244 python /vagrant/refinery/manage.py celeryd -c 1 -Q file_import --events',
+                '3350  0.2  4.0 82088 /usr/lib/erlang/erts-5.10.4/bin/beam -W w -A 64 -P 1048576 -t 5000000 -stbt db -zdbbl 32000 -K true -B i -- -root /usr/lib/erlang -progname erl -- -home /var/lib/rabbitmq -- -pa /usr'
+            ]
+        )
+
+    def test__get_iterations(self):
+        self.assertTrue(PsGrapher()._get_iterations(None))
+        self.assertTrue(PsGrapher(iterations=1)._get_iterations(0))
+        self.assertFalse(PsGrapher(iterations=10)._get_iterations(11))
+
+    def test__create_layout(self):
+        self.assertEqual(
+            PsGrapher()._create_layout(),
+            {'xaxis': {'rangeselector': {'buttons': [{'count': 1, 'step': 'minute', 'stepmode': 'backward', 'label': '1 min'}, {'count': 1, 'step': 'hour', 'stepmode': 'backward', 'label': '1 hour'}, {
+                'count': 1, 'step': 'day', 'stepmode': 'backward', 'label': '1 day'}, {'step': 'all'}]}, 'tickformat': '%H:%M:%S', 'type': 'date', 'rangeslider': {}}, 'title': 'Memory Usage (kb)'}
+        )
+
+    def test__get_graph_data(self):
+        self.assertEqual(self.ps_grapher._get_graph_data(), {})
+
+    def test__get_ps_data(self):
+        self.assertEqual(self.ps_grapher._get_ps_data(), {})
+
+    def test__update_graph_data(self):
+        self.ps_grapher._update_ps_data()
+        self.ps_grapher._update_graph_data()
+        self.assertEqual(
+            self.ps_grapher._get_graph_data(),
+            {
+                '5832 python /vagrant/refinery/manage.py runserver 0.0.0.0:8000 --noreload': {
+                    'opacity': 0.8,
+                    'name': '5832 python /vagrant/refinery/manage.py runserver 0.0.0.0:8000 --noreload',
+                    'connectgaps': True,
+                    'y': ['316144'],
+                    'x': [self.fake_datetime],
+                    'line': {'shape': 'spline'},
+                    'type': 'scatter'
+                },
+                '2382 java -server -Xss256k -Xms32m -Xmx32m': {
+                    'opacity': 0.8,
+                    'name': '2382 java -server -Xss256k -Xms32m -Xmx32m',
+                    'connectgaps': True,
+                    'y': ['143116'],
+                    'x': [self.fake_datetime],
+                    'line': {'shape': 'spline'},
+                    'type': 'scatter'
+                },
+                '1246 /usr/bin/java': {
+                    'opacity': 0.8,
+                    'name': '1246 /usr/bin/java',
+                    'connectgaps': True,
+                    'y': ['181116'],
+                    'x': [self.fake_datetime],
+                    'line': {'shape': 'spline'},
+                    'type': 'scatter'}
+            }
+        )
+
+    def test__update_ps_data(self):
+        self.ps_grapher._update_ps_data()
+        self.assertEqual(
+            self.ps_grapher._get_ps_data(),
+            {'5832 python /vagrant/refinery/manage.py runserver 0.0.0.0:8000 --noreload': {'trace': None, self.fake_datetime: '316144'}, '2382 java -server -Xss256k -Xms32m -Xmx32m': {
+                'trace': None, self.fake_datetime: '143116'}, '1246 /usr/bin/java': {'trace': None, self.fake_datetime: '181116'}}
+        )
+
+    def test__update_plotly_graph(self):
+        self.ps_grapher._update_ps_data()
+        self.ps_grapher._update_graph_data()
+        self.ps_grapher._update_plotly_graph()
 
 if __name__ == "__main__":
     unittest.main()
